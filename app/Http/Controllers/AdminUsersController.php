3c763e7a220d2ad361ac07bc7f81276caba64e0a
Junte <juntemedia@gmail.com>
2020-03-11 07:06:06 +0300

    Users section working 100 percent except security

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UsersEditRequest;
 use App\Http\Requests\UsersRequest;
 use App\Photo;
 use App\Role;
@@ -49,7 +50,15 @@ class AdminUsersController extends Controller
     {
         //
 
-        $input = $request->all();
+        if (trim($request->password) == ''){
+            $input = $request->except('password');
+        }else{
+            $input = $request->all();
+            $input = $request->except('password');
+            $input['password'] = bcrypt($request->password);
+        }
+
+
 
         if ($file = $request->file('photo_id')){
             $name = time().$file->getClientOriginalName();
@@ -62,13 +71,13 @@ class AdminUsersController extends Controller
 
         }
 
-        $input['password'] = bcrypt($request->password);
+
         User::create($input);
 
 
    // User::create($request->all());
 
-    //return redirect('/admin/users');
+    return redirect('/admin/users');
 
         //return $request->all();
 
@@ -98,6 +107,12 @@ class AdminUsersController extends Controller
     public function edit($id)
     {
         //
+
+        $user = User::findOrFail($id);
+
+        $roles = Role::lists('name','id')->all();
+
+        return view('admin.users.edit',compact('user','roles'));
     }
 
     /**
@@ -107,9 +122,35 @@ class AdminUsersController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UsersEditRequest $request, $id)
     {
         //
+            $user = User::findOrFail($id);
+
+        if (trim($request->password) == ''){
+            $input = $request->except('password');
+        }else{
+            $input = $request->all();
+            $input = $request->except('password');
+            $input['password'] = bcrypt($request->password);
+        }
+
+
+        if ($file = $request->file('photo_id')){
+                $name = time().$file->getClientOriginalName();
+
+                $file->move('images', $name);
+
+                $photo = Photo::create(['file'=>$name]);
+                $input['photo_id'] = $photo->id;
+            }
+
+            $user->update($input);
+
+            return redirect('/admin/users');
+
+
+
     }
 
     /**
--- app/Photo.php
+++ app/Photo.php
@@ -8,5 +8,13 @@ class Photo extends Model
 {
     //
 
+    protected $uploads = '/images/';
+
     protected $fillable = ['file'];
+
+
+
+    public function getFileAttribute($photo){
+        return $this->uploads . $photo; //this will always attach a photo and echo it out..we dont need to write to the images anymore
+    }
 }
